ed7abed8bf3fbfb0f3b6dc7542fa50197af6964c
vincentchung2002 <vincentchung2002@gmail.com>
2026-04-23 21:17:03 -0400

    part 1 etl

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -14,8 +14,6 @@ def create_directories(directories):
     Args:
         directories (list of str): A list of directory paths to create.
     """
-    
-    
     for directory in directories:
         os.makedirs(directory, exist_ok=True)
 
@@ -28,6 +26,7 @@ def extract_transform():
         - `arrest_events`: The dataframe containing arrest event data
         - `charge_counts`: A dataframe with counts of charges aggregated by charge degree
         - `charge_counts_by_offense`: A dataframe with counts of charges aggregated by both charge degree and offense category
+        - `pred_universe_merged`: pred_universe merged with felony_charge indicator
     """
     # Extracts arrest data CSVs into dataframes
     pred_universe = pd.read_csv('https://www.dropbox.com/scl/fi/a2tpqpvkdc8n6advvkpt7/universe_lab9.csv?rlkey=839vsc25njgfftzakr34w2070&dl=1')
@@ -36,5 +35,9 @@ def extract_transform():
     # Creates two additional dataframes using groupbys
     charge_counts = arrest_events.groupby(['charge_degree']).size().reset_index(name='count')
     charge_counts_by_offense = arrest_events.groupby(['charge_degree', 'offense_category']).size().reset_index(name='count')
-    
-    return pred_universe, arrest_events, charge_counts, charge_counts_by_offense
+
+    arrest_events['is_felony'] = arrest_events['charge_degree'].str.lower().str.contains('felony')
+    felony_charge = arrest_events.groupby('arrest_id')['is_felony'].any().reset_index()
+    felony_charge.columns = ['arrest_id', 'has_felony_charge']
+    pred_universe_merged = pred_universe.merge(felony_charge, on='arrest_id', how='left')
+    return pred_universe, arrest_events, charge_counts, charge_counts_by_offense, pred_universe_merged
